24ccac6cf44fde488e575faeebf4c49b0b987ea2
Farhan <Farhanf7n@gmail.com>
2021-10-03 14:40:29 +0500

    Corrected the fuction so it can dislay the passenger count on to the html page

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-// let count = 0
+let countEl = document.getElementById("count-el")
 
+let count = 0
 function increment(){
-    // count = count + 1
-    console.log("The button was clicked")
+   count  = count + 1
+   countEl.innerText = count
 }
